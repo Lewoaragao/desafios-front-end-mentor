0eb724821000ff205b0061987a53b098ffe822ae
Leonardo Aragão <lewoaragao@gmail.com>
2022-04-22 09:04:44 -0300

    atualizacao js

--- CALCULADORA-DE-GORJETAS/script.js
+++ CALCULADORA-DE-GORJETAS/script.js
@@ -1,15 +1,6 @@
-// var inputCustom = document.getElementById('input-custom')
 var porcentButtons = document.querySelectorAll('.porcent')
 var inputs = document.querySelectorAll('.input')
-var porcent = 0;
-
-// inputCustom.onfocus = () => {
-//     inputCustom.placeholder = ''
-// }
-
-// inputCustom.onblur = () => {
-//     inputCustom.placeholder = 'Custom'
-// }
+var txtAlertZero = document.getElementById('txtAlertZero')
 
 inputs[0].onfocus = () => {
     inputs[0].placeholder = ''
@@ -19,12 +10,16 @@ inputs[0].onblur = () => {
     inputs[0].placeholder = '0'
 }
 
+inputs[1].style.textAlign = 'center'
+
 inputs[1].onfocus = () => {
     inputs[1].placeholder = ''
+    inputs[1].style.textAlign = 'right'
 }
 
 inputs[1].onblur = () => {
     inputs[1].placeholder = 'Custom'
+    inputs[1].style.textAlign = 'center'
 }
 
 inputs[2].onfocus = () => {
@@ -33,4 +28,14 @@ inputs[2].onfocus = () => {
 
 inputs[2].onblur = () => {
     inputs[2].placeholder = '0'
+}
+
+inputs[2].onkeyup = () => {
+    if(inputs[2].value == '') {
+        txtAlertZero.style.display = 'none'
+    } else if(inputs[2].value == 0) {
+        txtAlertZero.style.display = 'block'
+    } else {
+        txtAlertZero.style.display = 'none'
+    }
 }
